600681319812e98f3ab52de2001080577180219a
Thanisorn33333 <139840240+Thanisorn33333@users.noreply.github.com>
2023-08-18 21:58:17 +0700

    Add files via upload

--- cal4.js
+++ cal4.js
@@ -1,46 +1,55 @@
+import React from 'react'
 import {View,Text,StyleSheet,Alert,Button,TextInput} from "react-native";
 
-export default function CalDemo(){
-        const t  = 'Click Count'
+export default function CalDemo4(){
+
+     
      
        let [inputNum1, setInputNum1] = React.useState(0)
        let [inputNum2, setInputNum2] = React.useState(0)
-       let[result,setResult] = React.useState(0)
-       let[p1,setp1] = React.useState(0)
-
-     const onPressButton1 =() => {
-        let num1= parseInt(inputNum1)
-        let num2= parseInt(inputNum2)
-        let num3= num1+num2
+       let [result,setResult] = React.useState(0)
+       let [resultg,setResultg] = React.useState(0)
        
-        
-        if(num3 <=75){
+     const onPressButton1 =() =>{
+        let a= parseInt(inputNum1)
+        let b= parseInt(inputNum2)
+        let rscore= a+b
+       
+        if(rscore >=75){
           msg ='good'
-        }else if (num3=>50){
+        }else if (rscore >=50){
           msg ='pass'
-        }else {
+        }else if (rscore >=49){
           msg ='fail'
+        }else{
+          msg = 'ใส่ค่าคะแนนให้ถูกต้อง'
         }
-        setResult(num3)
-        set1(msg)
-       }
-       }
-      
 
-      
-    
+        setResult(msg)
+        setResult(rscore)
+       
+       }
+
 return(
-
-    <View style={styles.container}>
+<View style={styles.container}>
         <View style={styles.item}>
-            <Text>โปรแกรมตรวจสอบคะแนนและแสดงเกรด</Text>
-  
+            <View><Text>โปรแกรมคำนวณเกรด</Text></View>
+              <View style={styles.item}>
+           <Text>คะแนนสอบ</Text>
+            <TextInput style={styles.textinput} defaultValue={inputNum1}onChange={event => setInputRum1(event.nativeEvent.text)}
+             keyboardType='number-pad'/>
             </View>
-            <View>
+            <View style={styles.item}>
+             <Text>คะแนนงาน</Text>
+             <TextInput style={styles.textinput} defaultValue={inputNum2}onChange={event => setInputRum1(event.nativeEvent.text)}
+             keyboardType='number-pad'/>
+                 </View>
+                  </View>
+             <View>
                 <Text>คะแนนรวม : {totalScore}</Text>
                 <Text>เกรด: {grade}</Text>
             </View>
-            </View>
+
     <View style={styles.item}>
        <Button title="คำนวณ" color = "#FF1493" onPress={ onPressButton1 }/>
       
@@ -76,3 +85,14 @@ const styles = StyleSheet.create({
     }
     
 })
+
+
+
+
+
+
+
+
+
+
+
